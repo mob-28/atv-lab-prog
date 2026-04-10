f2bfa4116cece5414ebdc149caaa6651bdfb5666
mob-28 <mateusobarboza07@gmail.com>
2026-04-10 11:43:27 -0300

    Alterações

--- atv-lab.c
+++ atv-lab.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-typedef struct alunos {
-    float n_teorico;
-    float n_pratico;
-    float aulas_assistidas;
-    struct alunos *prox;
-} aluno;
+struct Aluno {
+    float n_teorica;
+    float n_pratica;
+    int aulas_assistidas;
+};
 
 int menu () {
     int opcao;
@@ -21,8 +20,11 @@ int menu () {
 
 int main () {
     int opcao;
-    int qtd_aluno, aula_total_astd; 
-    float n_pratica, n_teorico;   
+    int qtd_aluno;
+    float maior_nota_pratica = 0, menor_nota_pratica = 11; 
+    float maior_nota_teorica = 0, menor_nota_teorica = 11; 
+
+    struct Aluno alunos[100];
 
     // tema, boas-vindas e integrantes do grupo
     printf("\n--- Sistema de Diagnostico de Turmas EPT ---\n");
@@ -34,7 +36,7 @@ int main () {
         switch (opcao) {
 
             case 1: 
-                // primeira opcao e faz a soliticacao de cada dado: solicita a quantidade de alunos na turma, nota de conhecimentos teóricos, a nota de habilidades práticas e o total de aulas assistidas
+                // primeira opcao e faz a soliticacao de cada dado: solicita a quantidade de alunos na turma, nota de conhecimentos teoricos, a nota de habilidades praticas e o total de aulas assistidas
 
                 printf("Digite a quantidade de alunos: ");
                 scanf("\n%d", &qtd_aluno);
@@ -43,19 +45,38 @@ int main () {
 
                     // pedido das infos de acordo com a quantidade de alunos
                     printf("Digite a nota de conhecimentos teoricos do aluno %d: ", i);
-                    scanf("\n%f", &n_teorico);
+                    scanf("\n%f", &alunos[i].n_teorica);
+                    
+                    // se nota digitada for maior que a guardada, o valor de nota eh a nova maior nota
+                    if (alunos[i].n_teorica > maior_nota_teorica) {
+                        maior_nota_teorica = alunos[i].n_teorica;
+                    }
+
+                    // se nota digitada for menor que a guardada, o valor da nota eh a nova menor nota
+                    if (alunos[i].n_teorica < menor_nota_teorica) {
+                        menor_nota_teorica = alunos[i].n_teorica;
+                    }
 
                     printf("Digite a nota de habilidades praticas do aluno %d: ", i);
-                    scanf("\n%f", &n_pratica);
+                    scanf("\n%f", &alunos[i].n_pratica);
+
+                    // se nota digitada for maior que a guardada, o valor de nota eh a nova maior nota
+                    if (alunos[i].n_pratica > maior_nota_pratica) {
+                        maior_nota_pratica = alunos[i].n_pratica;
+                    }
+
+                    // se nota digitada for menor que a guardada, o valor da nota eh a nova menor nota
+                    if (alunos[i].n_pratica < menor_nota_pratica) {
+                        menor_nota_pratica = alunos[i].n_teorica;
+                    }
 
                     printf("Digite a quantidade de aulas assistidas do aluno %d: ", i);
-                    scanf("\n%d", &aula_total_astd);
+                    scanf("\n%d", &alunos[i].aulas_assistidas);
                 }
-
-                // pede novamente uma opcao
-                menu();
-                printf("Digite uma opcao: ");
-                scanf("%d", &opcao);
+                
+                printf("%f\n", maior_nota_pratica);
+                printf("%f\n", menor_nota_pratica);
+                opcao = menu();
 
         }
     } while (opcao != 4); // repete ate que a opcao 4 seja requisitada
